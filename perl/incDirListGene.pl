b91cf5c26e09ecab2cd2ae06d2f60287e1869d05
Linshizhi 0848 <463551449@qq.com>
2017-09-07 16:40:10 +0800

    Update.

--- perl/incDirListGene.pl
+++ perl/incDirListGene.pl
@@ -3,6 +3,16 @@ use warnings;
 
 use Path::Tiny;
 use Cwd;
+use Encode;
+use Data::Dumper;
+use JSON;
+
+# Where to begin and recursive down.
+my $ROOT_PATH = @ARGV[0];
+# Value of macro or some variable.
+my $VAR_LIST_PATH = @ARGV[1];
+# The name of files that we want to process.
+my $TARGET_FILE= @ARGV[2];
 
 sub iterator {
     my $DIR_PATH = $_[0];
@@ -14,11 +24,12 @@ sub iterator {
     my @filelist = readdir DIR;
     
     chdir $DIR_PATH;    
-    
+       
     foreach $dir (@filelist) {        
         if ($dir eq "." || $dir eq "..") {
             next;
         }
+        
         if (-f $dir) {
             # Text-files
             &$deal_with($dir);                 
@@ -35,23 +46,39 @@ sub iterator {
 sub deal_with_file {    
     my $file = $_[0];
     my $row = undef;
+    my $match = undef;
+    my $variable = undef;
+    my $substitute_key = undef;
+    my $substitute_val = undef;
     
-    if ($_[0] ne "Makefile") {
+    if ($_[0] ne $TARGET_FILE) {
        return;
     }
     
     open FILE, $file;
     
     while ($row = <FILE>) {
-        print $row;
+        # Content of -I match
+        if($row =~ m/(-I[^\\\n ]*)/i) {
+            $match = $1;            
+        }
+        
+        # substitute content of variable 
+        # into Content of -I option.
+        if ($row =~ m/\$\(([^\)]*)\)/) {
+            $substitute_key = $1;
+            $substitute_val = # Get from JSON.
+            $row =~ s/\$\($substitute\)/$substitute_val/;
+        }
     }
-    
-    # Filter out the PRE_INCLUDE content.
-    
-    # Generate include.lnt file.
     
     close FILE;
 }
 
+# subroutine to generate include.lnt
+sub include_lnt_gen {
+    
+}
+
 # main
-&iterator("E:\\CODE\\GL8600\\gbn\\src", \&deal_with_file);
+&iterator($ROOT_PATH, \&deal_with_file);
